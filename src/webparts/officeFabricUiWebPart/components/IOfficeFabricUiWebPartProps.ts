fe2a29edcd659d7baff0ea5c298afeaeca847a9c
saurav kumar <saurav.kumar185@gmail.com>
2025-07-02 13:57:15 +0530

    first commit

---
+++ src/webparts/officeFabricUiWebPart/components/IOfficeFabricUiWebPartProps.ts
@@ -0,0 +1,7 @@
+export interface IOfficeFabricUiWebPartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
