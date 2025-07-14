f5b001bac7e9dfcfefc73eaf603f8da1f42fae55
saurav kumar <saurav.kumar185@gmail.com>
2025-07-14 13:54:00 +0530

    Update

--- src/webparts/officeFabricUiWebPart/components/OfficeFabricUiWebPart.tsx
+++ src/webparts/officeFabricUiWebPart/components/OfficeFabricUiWebPart.tsx
@@ -1,43 +1,71 @@
-import * as React from 'react';
-import styles from './OfficeFabricUiWebPart.module.scss';
-import type { IOfficeFabricUiWebPartProps } from './IOfficeFabricUiWebPartProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+//import styles from './OfficeFabricUiWebPart.module.scss';
+import type { IOfficeFabricUiWebPartProps } from "./IOfficeFabricUiWebPartProps";
+//import { escape } from "@microsoft/sp-lodash-subset";
+import { Dropdown, TextField, DropdownMenuItemType } from "@fluentui/react";
 
 export default class OfficeFabricUiWebPart extends React.Component<IOfficeFabricUiWebPartProps> {
   public render(): React.ReactElement<IOfficeFabricUiWebPartProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
-
     return (
-      <section className={`${styles.officeFabricUiWebPart} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
+      <>
+        <TextField
+          label="Name"
+          iconProps={{ iconName: "people" }}
+          placeholder="Enter your name"
+        />
+        <TextField
+          label="Email"
+          iconProps={{ iconName: "mail" }}
+          placeholder="Enter your email"
+          type="email"
+        />
+        <TextField
+          label="Email"
+          iconProps={{ iconName: "mail" }}
+          placeholder="Enter your email"
+          type="email"
+        />
+        <TextField label="Password" type="password" />
+        <TextField label="File" type="file" />
+        <TextField label="Salary" prefix="$" suffix="USD" />
+        <TextField label="Error" errorMessage="I am error" disabled={true} />
+        <TextField
+          label="Address"
+          multiline
+          rows={5}
+          iconProps={{ iconName: "home" }}
+        />
+        <Dropdown
+          options={[
+            {
+              key: "Header",
+              text: "Options",
+              itemType: DropdownMenuItemType.Header,
+            },
+            { key: "A", text: "Option a", data: { icon: "Memo" } },
+            { key: "B", text: "Option b", data: { icon: "Print" } },
+            { key: "C", text: "Option c", data: { icon: "ShoppingCart" } },
+            { key: "D", text: "Option d", data: { icon: "Train" } },
+            { key: "E", text: "Option e", data: { icon: "Repair" } },
+            {
+              key: "divider_2",
+              text: "-",
+              itemType: DropdownMenuItemType.Divider,
+            },
+            {
+              key: "Header2",
+              text: "More options",
+              itemType: DropdownMenuItemType.Header,
+            },
+            { key: "F", text: "Option f", data: { icon: "Running" } },
+            { key: "G", text: "Option g", data: { icon: "EmojiNeutral" } },
+            { key: "H", text: "Option h", data: { icon: "ChatInviteFriend" } },
+            { key: "I", text: "Option i", data: { icon: "SecurityGroup" } },
+            { key: "J", text: "Option j", data: { icon: "AddGroup" } },
+          ]}
+          label="Dropdown with icons"
+        />
+      </>
     );
   }
 }
